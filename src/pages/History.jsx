5f7b1a699e77ed5af6618d9072356b63f93f4c3f
masihgh <masihprogrammer@gmail.com>
2023-03-18 18:03:50 +0330

    Remove Some Logs And New Log Component

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -15,31 +15,6 @@ function History() {
               <div className="text-xs">You have 1 unread message</div>
             </div>
           </div>
-          <div className="flex-none">
-            <button className="btn btn-sm">See</button>
-          </div>
-        </div><div className="alert shadow-lg mt-5">
-          <div>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info flex-shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-            <div>
-              <h3 className="font-bold">New message!</h3>
-              <div className="text-xs">You have 1 unread message</div>
-            </div>
-          </div>
-          <div className="flex-none">
-            <button className="btn btn-sm">See</button>
-          </div>
-        </div><div className="alert shadow-lg mt-5">
-          <div>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info flex-shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-            <div>
-              <h3 className="font-bold">New message!</h3>
-              <div className="text-xs">You have 1 unread message</div>
-            </div>
-          </div>
-          <div className="flex-none">
-            <button className="btn btn-sm">See</button>
-          </div>
         </div>
 
       </div>
