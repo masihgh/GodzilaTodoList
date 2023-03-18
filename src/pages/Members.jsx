1ee175795d87d1594b52ff2c4c03c2baee408bed
masihgh <masihprogrammer@gmail.com>
2023-03-18 17:30:46 +0330

    Replace class to ClassName, import Skill Component

--- src/pages/Members.jsx
+++ src/pages/Members.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
+import { Skills } from "../features/members/Skills";
 import MainLayout from "../layouts/MainLayout"
 
 function Members() {
+  
   const [memberData, setMemberData] = useState({
     name: "",
     age: "",
@@ -11,8 +13,11 @@ function Members() {
     language: "",
     skills: {},
   });
+
   const handleSubmit = (e) => {
+    
     e.preventDefault();
+    console.log(memberData);
     // const data = new FormData();
     // data.append('file', memberData.avatar)
     // console.warn(memberData.avatar);
@@ -26,17 +31,17 @@ function Members() {
     <MainLayout>
       <div className="card bg-base-300 shadow-xl p-8 ">
         <section className="card-actions mx-auto pb-4">
-          <button class="btn gap-2">
+          <button className="btn gap-2">
             Sudo
-            <div class="badge badge-warning">MasihGh</div>
+            <div className="badge badge-warning">MasihGh</div>
           </button>
-          <button class="btn gap-2">
+          <button className="btn gap-2">
             Members
-            <div class="badge badge-secondary">12</div>
+            <div className="badge badge-secondary">12</div>
           </button>
           <label htmlFor="modal-new-member" className="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 25 25" strokeWidth={1.8} stroke="currentColor" className="w-5 h-5"><path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" /></svg> Create New Member</label>
         </section>
-        <div class="divider">Members</div>
+        <div className="divider">Members</div>
 
       </div>
       <input type="checkbox" id="modal-new-member" className="modal-toggle" />
@@ -76,13 +81,13 @@ function Members() {
                 </label>
                 <input onChange={(e) => setMemberData({ ...memberData, avatar: e.target.files[0] })} type="file" className="file-input file-input-bordered file-input-sm w-full" />
               </div>
-              <div className="form-control w-full mb-6">
+              {/* <div className="form-control w-full mb-6">
                 <label className="label">
                   <span className="label-text">Skills</span>
                 </label>
-                <label class="input-group">
+                <label className="input-group">
                   <button className="btn-primary btn btn-sm">Add</button>
-                  <input type="text" placeholder="info@site.com" class="input input-bordered input-sm w-full" />
+                  <input type="text" placeholder="info@site.com" className="input input-bordered input-sm w-full" />
                 </label>
               </div>
               <div className="mb-6 gap-3">
@@ -95,7 +100,8 @@ function Members() {
                   success
                 </div>
                 
-              </div>
+              </div> */}
+              <Skills />
               <div className="form-control w-full mb-4">
                 <label className="label">
                   <span className="label-text">Language</span>
