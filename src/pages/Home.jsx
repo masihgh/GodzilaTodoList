679f5ba91c61be621dc4d884d88b56812181c2c3
masihgh <masihprogrammer@gmail.com>
2023-03-30 21:47:47 +0330

    import useEffect from React

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import Title from "../components/Title";
 import MainLayout from "../layouts/MainLayout"
 
