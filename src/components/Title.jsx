30ff3c684c035746c033116a95d6e34dc6b0418c
masihgh <masihprogrammer@gmail.com>
2023-03-30 21:44:54 +0330

    Add Margin Top & Bouttom To Title Divider

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,6 +1,6 @@
 function Title({title,value}) {
     return (
-        <div className="divider">{title}
+        <div className="divider mb-8 mt-4">{title}
             <button className="btn gap-2">
                 <div className="badge badge-secondary">{value}</div>
             </button>
