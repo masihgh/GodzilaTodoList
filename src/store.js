be0bcbd25960df4af2c6d11ea588c0ebd92cc0b9
masihgh <masihprogrammer@gmail.com>
2023-04-05 17:55:47 +0330

    Change Tasks To Home

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import skillsReducer from './features/members/skillsSlice'
+import tasksReducer from './features/home/TasksSlice'
 
 export default configureStore({
   reducer: {
-    skills: skillsReducer,  
+    skills: skillsReducer,
+    tasks: tasksReducer
 },
 })
