309778aff8a61ce92ba6fc076e37ae1ed7a849d9
masihgh <masihprogrammer@gmail.com>
2023-04-05 17:52:31 +0330

    Add Create API

--- src/api/MemberAPI.js
+++ src/api/MemberAPI.js
@@ -1,6 +1,11 @@
 import API from '../axiosConfig';
+import authHeader from '../services/auth-header';
 
 const API_URL = "/member";
+
+export const CreateMember = async (member) => {
+    return API.post(API_URL + "/",member, { headers: authHeader() });
+};
 
 export const getAllMembers = async () => {
     return API.get(API_URL + "/list");
--- src/api/TaskAPI.js
+++ src/api/TaskAPI.js
@@ -4,6 +4,11 @@ import authHeader from "../services/auth-header";
 
 const API_URL = "/task";
 
+export const CreateTask = async (task) => {
+    return API.post(API_URL + "/",task, { headers: authHeader() });
+};
+
+
 export const getAllTasks = async () => {
     return API.get(API_URL + "/", { headers: authHeader() });
 };
