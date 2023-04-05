5cce5190a36f4447845d75a4f4ac61e89f527ebe
masihgh <masihprogrammer@gmail.com>
2023-04-05 17:44:26 +0330

    add react-toastify and init

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,23 @@
 import NavigationMenu from "../components/NavigationMenu";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function MainLayout(props) {
     return (
         <div className="container mx-auto px-4 pt-5">
-            <main className="mb-20">
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="dark"
+            />
+            <main data-theme="dark" className="mb-20">
                 {props.children}
             </main>
             <NavigationMenu />
