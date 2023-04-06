594e9f51e4c781327ea93f567a9a0a2e8f8d6e42
masihgh <masihprogrammer@gmail.com>
2023-04-06 23:21:38 +0330

    add tasks and members Reducer

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import skillsReducer from './features/members/skillsSlice'
 import tasksReducer from './features/home/TasksSlice'
+import membersReducer from './features/members/membersSlice'
 
 export default configureStore({
   reducer: {
     skills: skillsReducer,
-    tasks: tasksReducer
+    tasks: tasksReducer,
+    members: membersReducer
 },
 })
